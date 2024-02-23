867fa3d28aa0e4fbefbb1a1b268bbefe759614e7
Matheus Figueiredo <108003682+mfigueireddo@users.noreply.github.com>
2024-02-23 19:42:54 -0300

    Calculadora e jogo da forca criados

--- calculadora-simples.py
+++ calculadora-simples.py
@@ -13,82 +13,107 @@ def eh_numero(string):
     return True
 
 def eh_operador(string):
+
     operadores = ['+','-','*','/','**','//','%']
+
     if string not in operadores:
         return False
+    
     return True
 
+def valida_numero(numero):
+
+    while eh_numero(numero) ==  False:
+
+        print("\nInsira apenas números")
+        numer = input("Insira um número: ")
+
+    return numero
+
+def valida_operador(operador):
+
+    while eh_operador(operador) ==  False:
+        print("\nInsira um operador válido.")
+        operador = input("Insira o operador: ")
+
+    return operador
+
 def soma(numero_1,numero_2):
+
     return numero_1 + numero_2
 
 def subtracao(numero_1,numero_2):
+
     return numero_1 - numero_2
 
 def multiplicacao(numero_1,numero_2):
+
     return numero_1 * numero_2
 
 def divisao(numero_1,numero_2):
+
     return numero_1 / numero_2
 
 def potenciacao(numero_1,numero_2):
+
     return numero_1 ** numero_2
 
 def parte_inteira(numero_1,numero_2):
+
     return numero_1 // numero_2
 
 def resto(numero_1,numero_2):
+
     return numero_1 % numero_2
 
-# Validação do número 1
+def contas(numero_1,numero_2,operador):
 
+    if operador == '+':
+        resultado = soma(numero_1,numero_2)
+
+    elif operador == '-':
+        resultado = subtracao(numero_1,numero_2)
+    
+    elif operador == '*':
+        resultado = multiplicacao(numero_1,numero_2)
+    
+    elif operador == '/':
+        resultado = divisao(numero_1,numero_2)
+    
+    elif operador == '**':
+        resultado = potenciacao(numero_1,numero_2)
+    
+    elif operador == '//':
+        resultado = parte_inteira(numero_1,numero_2)
+    
+    elif operador == '%':
+        resultado = resto(numero_1,numero_2)
+
+    return resultado
+
+# Pede o primeiro número
 numero_1 = input("Insira um número: ")
+numero_1 = valida_numero(numero_1)
 
-while eh_numero(numero_1) ==  False:
-    print("\nInsira apenas números")
-    numero_1 = input("Insira um número: ")
-
-# Validação do operador
-
+# Pede o operador
 print("\nLista de operadores: +, -, *, /, **, //, %")
 operador = input("Insira o operador: ")
+operador = valida_operador(operador)
 
-while eh_operador(operador) ==  False:
-    print("\nInsira um operador válido.")
-    operador = input("Insira o operador: ")
-
-# Validação do número 2
-
+# Pede o segundo número
 numero_2 = input("\nInsira outro número: ")
+numero_2 = valida_numero(numero_2)
 
-while eh_numero(numero_2) ==  False:
-    print("\nInsira apenas números.")
-    numero_2 = input("Insira outro número: ")
-
+# Confere se não está sendo realizada uma divisão por 0
 if operador == "/" or operador == '//' or operador == '%':
     while float(numero_2) == 0:
         print("\nNão é possível realizar operações de divisão por 0.")
         numero_2 = input("Insira outro número: ")
 
 # Contas
-        
 numero_1 = float(numero_1)
 numero_2 = float(numero_2)
     
-if operador == '+':
-    resultado = soma(numero_1,numero_2)
-elif operador == '-':
-    resultado = subtracao(numero_1,numero_2)
-elif operador == '*':
-    resultado = multiplicacao(numero_1,numero_2)
-elif operador == '/':
-    resultado = divisao(numero_1,numero_2)
-elif operador == '**':
-    resultado = potenciacao(numero_1,numero_2)
-elif operador == '//':
-    resultado = parte_inteira(numero_1,numero_2)
-elif operador == '%':
-    resultado = resto(numero_1,numero_2)
-
-# Imprimindo o resultado
-    
+# Resultado
+resultado = contas(numero_1,numero_2,operador)
 print("\n%.2f %s %.2f = %.2f" %(numero_1,operador,numero_2,resultado))
---
+++ jogo-da-forca.py
@@ -0,0 +1,123 @@
+import random
+
+def escolha_palavra():
+
+    palavras = ["rinoceronte","basquete","ventilador"]
+    palavra_secreta = random.choice(palavras)
+
+    return palavra_secreta
+
+def indices(palavra_secreta,chute):
+
+    # Cria uma lista que irá retornar o(s) índice(s) do chute na palavra secreta
+    indices = []
+    auxiliar_indice = -1
+    contador = 0
+
+    # Conta quantas vezes o chute apareceu na palavra
+    frequencia = palavra_secreta.count(chute)
+
+    while contador < frequencia:
+        # Adiciona na lista o índice do chute na palavra (podendo ser mais de uma)
+        indices += [palavra_secreta.find(chute,auxiliar_indice+1)]
+        auxiliar_indice = indices[contador]
+        contador += 1
+
+    # Retorna uma lista com os índices
+    return indices
+
+def valida_chute(chute,letras_chutadas):
+
+    while chute in letras_chutadas:
+        print("\nVocê já chutou esta letra.")
+        chute = input("Chute uma letra: ")
+
+    return chute
+
+def acerto(chute,palavra_secreta,desenho):
+
+    print("%s está na palavra!" %(chute))
+
+    # Chama a função que retorna o(s) índice(s) onde aparece o chute
+    lista = indices(palavra_secreta,chute)
+
+    # Percorre a lista de índice(s)
+    for elemento in lista:
+
+        # Transforma a letra em maiúscula caso seja a primeira letra da palavra
+        if elemento == 0:
+            chute = chute.upper()
+
+        # Altera o _ pelo chute
+        desenho[elemento] = chute
+
+    return desenho
+
+def erro(chances):
+
+    print("\n%s não está na palavra." %(chute))
+
+    return chances - 1
+
+def informativo(chances,desenho,letras_chutadas):
+
+    print("\nVocê tem %d chances!" %(chances))
+
+    print(*desenho)
+
+    print("Letras chutadas: ", end="")
+    print(*letras_chutadas, sep=" ")
+
+def resultado(vitoria):
+
+    if vitoria == 1:
+        print("\nParabéns, você ganhou!")
+        print("Você adivinhou a palavra %s em %d chutes!" %(palavra_secreta, quantidade_chutes))
+
+    else :
+        print("\nVocê perdeu.")
+        print("A palavra correta era %s." %(palavra_secreta))
+
+palavra_secreta = escolha_palavra()
+
+vitoria = 0
+
+tamanho = len(palavra_secreta)
+chances = tamanho/2
+
+letras_chutadas = []
+quantidade_chutes = 0
+
+# Cria uma string com _ para representar espaços vazios na palavra
+desenho = ["_"] * tamanho
+
+print("Você tem %d chances!" %(chances))
+print(*desenho)
+
+while chances != 0:
+    chute = input("\nChute uma letra: ")
+    chute = chute.lower()
+
+    chute = valida_chute(chute,letras_chutadas)
+
+    quantidade_chutes += 1
+    letras_chutadas += chute
+
+    # Em caso de acerto
+    if chute in palavra_secreta:
+        desenho = acerto(chute,palavra_secreta,desenho)
+
+    # Em caso de erro
+    else:
+        chances = erro(chances)
+
+    # Revela o usuário quantas chances lhe restaram, o estado do desenho e as letras chutadas
+    informativo(chances,desenho,letras_chutadas)
+
+    # Condição de vitória
+    if "_" not in desenho:
+        vitoria = 1
+        break
+
+# Imprime o resultado
+resultado(vitoria)
